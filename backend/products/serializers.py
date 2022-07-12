f1de8ac9b9ef41a71e239ca1dc0ef1934b2fa237
RajrupDasid <rajrupdasid@outook.com>
2022-07-12 20:59:23 +0530

    RA-1 RA-3 'serializers and model serializers'

---
+++ backend/products/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    client_discount=serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model=Product
+        fields=[
+                'title',
+                'content',
+                'price',
+                'sale_price',
+                'client_discount',
+            ]
+
+    def get_client_discount(self,obj):
+        try:
+            return obj.get_discount()
+        except:
+            return None
