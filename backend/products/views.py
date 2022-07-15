c3ce752bf0804e706ca2505c3118650ba3399eec
RajrupDasid <rajrupdasid@outook.com>
2022-07-15 21:01:10 +0530

    RA-1 RA-2 'create api added'

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,6 +3,23 @@ from .models import Product
 from .serializers import ProductSerializer
 
 
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def prtform_create(self, serializer):
+        print(serializer.validate_data)
+        title = serializer.validate_data.get('title')
+        content = serializer.validate_data.get('content')
+        # or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
+
+product_create_view = ProductCreateAPIView.as_view()
+
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
