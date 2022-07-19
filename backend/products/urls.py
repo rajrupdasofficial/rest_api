b2ddf7f9f21f6ae1bfff1490d5f72311904847e5
RajrupDasid <rajrupdasid@outlook.com>
2022-07-19 21:36:23 +0530

    some typo fixed and new url has added

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ProductDetailAPIView, product_create_view
+from .views import ProductDetailAPIView, product_list_create_view
 
 urlpatterns = [
-    path('', product_create_view),
+    path('', product_list_create_view),
     path('<int:pk>/', ProductDetailAPIView.as_view())
 
 ]
