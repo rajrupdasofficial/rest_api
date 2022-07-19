b0b636c1ad895535b88144fa4d384b07f1e43941
RajrupDasid <rajrupdasid@outlook.com>
2022-07-19 21:37:15 +0530

    general views added

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,9 +1,11 @@
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
-
-class ProductCreateAPIView(generics.CreateAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
@@ -17,9 +19,32 @@ class ProductCreateAPIView(generics.CreateAPIView):
         serializer.save(content=content)
 
 
-product_create_view = ProductCreateAPIView.as_view()
+product_list_create_view = ProductListCreateAPIView.as_view()
 
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+product_list_view = ProductListAPIView.as_view()
+
+@api_view(['GET','POST'])
+def product_alt_view(request, *args, **kwargs):
+    method = request.method
+
+    if method == "GET":
+        pass
+        queryset=Product.objects.all()
+        data=ProductSerializer(queryset,many=True).data
+        return Response(data)
+
+    if method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid (raise_exception=True):
+            print(serializer.data)
+            return Response(serializer.data)
+    return Response({"invalid" : "invalid error"}, status=400)
